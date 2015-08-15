f1dc67f49ac60a605ebc7d066f1de16718187b69
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2015-08-15 16:14:46 +0300

    Use strict validation for Point, add method to_param

--- models/point.rb
+++ models/point.rb
@@ -1,6 +1,12 @@
 class Point < ActiveRecord::Base
   has_many :addresses
 
-  validates :latitude, presence: true, format: { with: /\A[-+]?\d{1,2}[.]?\d*\z/ }
-  validates :longitude, presence: true, format: { with: /\A[-+]?\d{1,3}[.]?\d*\z/ }
+  validates :latitude, presence: true, format: { with: /\A[-+]?\d{1,2}[.]\d+\z/ }
+  validates :longitude, presence: true, format: { with: /\A[-+]?\d{1,3}[.]\d+\z/ }
+
+  def to_param(reverse = false)
+    params = [longitude, latitude]
+    params.reverse! if reverse
+    params.join ','
+  end
 end
